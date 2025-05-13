320d6ba597f8db335ecf70f1a923cab1388b5082
Microwave <microwave@Microwaves-MacBook-Air.local>
2025-05-13 18:55:59 +0700

    firstproject javascript

---
+++ FirstprojectJavaScript/script.js
@@ -0,0 +1,25 @@
+
+//ตัวอย่างก่ารใช้ var,let และ const
+/*var greeting = "hello world";
+let status = true;
+const pi = 3.14;
+console.log(greeting);
+console.log(status);
+console.log(pi);
+*/
+
+/* ตัวอย่างการใช้ ประกาศตัวแปร และแก้ไขค่าตัวแปร
+let mynumber = 8;
+mynumber = 18;
+console.log(mynumber);
+*/
+
+/*การประกาศตัวแปรด้วยคีย์เวิร์ด const
+const mynumber = 8;
+mynumber = 18;
+console.log(mynumber);
+*/
+
+
+
+
